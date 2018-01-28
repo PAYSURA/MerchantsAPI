9bd96185b80a7ec86403f8ef6590245e6a1de39d
Jahn Zaib Khan <jahnzaib.khan2@ksb.com>
2018-01-28 18:58:18 +0100

    Feature-branch-Merchants  yaml file setting. Test cases

---
+++ src/test/java/com/paysura/merchants/service/AlgorithmTest.java
@@ -0,0 +1,24 @@
+package com.paysura.merchants.service;
+
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AlgorithmTest {
+
+  @Before
+  public void setUp() throws IOException {
+  }
+
+  @Test
+  public void testAlgorithm() throws IOException {
+		//assertEquals(response, "response");
+  }
+
+}
+ 
