07e0da652e4d4efd48cbf2dcdf9ec9c8726f9d21
Jahn Zaib Khan <jahn-zaib.khan@outlook.de>
2018-02-02 18:00:13 +0100

    Merchants-Feauture-branch Parser for Merchants data Model conversion to string

--- src/main/java/com/paysura/merchants/helper/Parser.java
+++ src/main/java/com/paysura/merchants/helper/Parser.java
@@ -1,5 +1,26 @@
 package com.paysura.merchants.helper;
 
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.paysura.merchants.model.MerchantsRequestModel;
+
 public class Parser {
+
+	public static String convertToString(MerchantsRequestModel merchantsRequest) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		String merchantRequestAsString = null;
+		merchantRequestAsString = mapper.writeValueAsString(merchantsRequest);
+		return merchantRequestAsString;
+	}
+
+	public static MerchantsRequestModel unconvertToRequestModel(String merchants) throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		MerchantsRequestModel merchantsRm = mapper.readValue(merchants, MerchantsRequestModel.class);
+		return merchantsRm;
+	}
 
 }
